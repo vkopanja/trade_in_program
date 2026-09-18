3e3f0d1afe4e2b1f5296c7af5b9c66e5eca9e305
Vedran Kopanja <vedran@kopanja.rs>
2026-09-18 11:50:52 +0200

    Updated __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -11,13 +11,10 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Vedran Kopanja",
+    'website': "https://github.com/vkopanja",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
